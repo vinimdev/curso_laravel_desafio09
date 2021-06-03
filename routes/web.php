3587da6cbded1e0e1b3a9db32c03ba426013f438
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-06-03 16:23:14 -0300

    Inserido redirecionamento da pagina de raiz para pagina de produtos

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/product', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::get('/', function () {
+    return redirect()->route('product.index');
+});
+Route::get('/product', [\App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
 Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
